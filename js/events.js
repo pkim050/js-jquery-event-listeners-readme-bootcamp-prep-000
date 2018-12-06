6d8d8ef25370fe45d5d27b2c7bdfc99e2a5bc58c
Patrick A Kim <pkim050@gmail.com>
2018-12-06 15:40:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt() {
+  $('.tasty').on("load", function() {
+    
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("G has been pressed.");
+      return;
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() { 
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
 
