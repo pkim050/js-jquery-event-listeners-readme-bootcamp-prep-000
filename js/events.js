efcbfe8a15c16d3c7e484af106346d9c5a194bf4
Patrick A Kim <pkim050@gmail.com>
2018-12-12 13:49:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+    $('img').css('border', "solid 2px red");
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("G has been pressed.");
+      return;
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() { 
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIT();
+pressIt();
+submitIt();
 });
